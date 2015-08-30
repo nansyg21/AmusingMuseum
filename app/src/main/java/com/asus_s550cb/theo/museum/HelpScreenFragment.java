1d082d9400f54a2dc4ed32020c51952a6e647452
nansyg21 <it12148@uom.edu.gr>
2015-08-30 20:42:00 +0300

    Initialize help screen

---
+++ app/src/main/java/com/asus_s550cb/theo/museum/HelpScreenFragment.java
@@ -0,0 +1,22 @@
+package com.asus_s550cb.theo.museum;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HelpScreenFragment extends Fragment
+{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.help_screen_fragment, container, false);
+
+        return rootView;
+    }
+
+
+
+}
